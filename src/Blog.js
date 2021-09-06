3f780dc735dd38b996a0967643f89355ac9652b8
yusuf <mugaluyusu35@gmail.com>
2021-09-06 15:27:16 +0300

    Added feature-2 and some other map and filter methods

--- src/Blog.js
+++ src/Blog.js
@@ -5,9 +5,9 @@ const Blogs = () => {
         {title:'How to Venture into Clothes business', body:'Conduct market research. Market research will tell you if theres an opportunity to turn your idea into a successful business',author:'cliff ivan',id:1},
         {title:'How to Venture into Crocks business', body:'Conduct market research. Market research will tell you if theres an opportunity to turn your idea into a successful business',author:'Milphat',id:2},
         {title:'How to Venture into Juice business', body:'Conduct market research. Market research will tell you if theres an opportunity to turn your idea into a successful business',author:'myusuf',id:3},
-        {title:'How to Venture into Shoes business', body:'Conduct market research. Market research will tell you if theres an opportunity to turn your idea into a successful business',author:'Madrin',id:4},
+        {title:'How to Venture into Shoes business', body:'Conduct market research. Market research will tell you if theres an opportunity to turn your idea into a successful business',author:'myusuf',id:4},
         {title:'How to Venture into  Ariobes business', body:'Conduct market research. Market research will tell you if theres an opportunity to turn your idea into a successful business',author:'katenta',id:5},
-        {title:'How to Venture into Perfumes/scents business', body:'Conduct market research. Market research will tell you if theres an opportunity to turn your idea into a successful business',author:'',id:6},
+        {title:'How to Venture into Perfumes/scents business', body:'Conduct market research. Market research will tell you if theres an opportunity to turn your idea into a successful business',author:'myusuf',id:6},
         {title:'How to Venture into the Bonnents business', body:'Go to jemba plaza buy stuff and start working on the bonnets',author:'Mariam K',id:7},
 
     ])
@@ -15,6 +15,7 @@ const Blogs = () => {
         <div className="home">
              <p>{'Blogs Here'}</p><br />
             <BlogList blogers ={blogs}/>
+            <BlogList blogers ={blogs.filter((blog)=>blog.author==='myusuf')} name ='myusuf Blogs!'/>
         </div>
 
      );
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,15 @@
-const BlogList = (props) => {
-    const blogs = props.blogers;
+const BlogList = ({blogers,name}) => {
+    // const blogs = props.blogers;
+    // const name = props.name;
 
-    console.log(blogs);
-    console.log(props);
+    // console.log(name);
+
+    // console.log(blogs);
+    // console.log(props);
     return ( 
         <div className="blog-lists">
-            {blogs.map((blog)=>(
+            <h2>{name}</h2>
+            {blogers.map((blog)=>(
                 <div className="blogs-preview" key>
                     <h2>{blog.title}</h2><br />
                     <p>{blog.body}</p><br />
